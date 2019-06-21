85e2b9447ba9561a24beba74a9b4dc38fce7f60b
aaamarkin <aaamarkin@gmail.com>
2019-06-21 16:20:13 +0300

    Initial commit

---
+++ GameTaskRemote.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UniRx;
+
+namespace Common.GameTask
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// Отложенная задача. Принимает замыкание, которое будет вызвано в момент старта задачи.
+	/// </summary>
+	public class GameTaskRemote : IGameTask
+	{
+		private readonly Func<IGameTask> _closure;
+		private readonly BoolReactiveProperty _complete = new BoolReactiveProperty(false);
+
+		private IDisposable _completeHandler;
+		private IGameTask _gameTask;
+
+		public GameTaskRemote(Func<IGameTask> closure)
+		{
+			_closure = closure;
+		}
+
+		// ITask
+
+		public void Start()
+		{
+			if (_gameTask != null) return;
+			_gameTask = _closure.Invoke();
+			_completeHandler = _gameTask.Complete.Subscribe(value =>
+			{
+				_complete.SetValueAndForceNotify(value);
+				if (!value) return;
+				_completeHandler.Dispose();
+				_completeHandler = null;
+			});
+			_gameTask.Start();
+		}
+
+		IReadOnlyReactiveProperty<bool> IGameTask.Complete => _complete;
+
+		// \ITask
+	}
+}
